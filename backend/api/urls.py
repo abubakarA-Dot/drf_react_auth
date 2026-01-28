51d7a47d5c4cd639a2c36b1be1e1485e867a85a2
abubakar <abubakarbryar@gmail.com>
2026-01-28 04:03:55 +0300

    backend user register endpoint. working

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoginViewSet
+from .views import (AuthenticationViewSet, RegisterViewSet)
 
 # router = DefaultRouter()
 # router.register(r'auth', LoginViewSet, basename='auth')
 
 urlpatterns = [
-    path('login/', LoginViewSet.as_view(), name="login"),
+    path('login/', AuthenticationViewSet.as_view(), name="login"),
+    path('register/', RegisterViewSet.as_view({'post': 'create'}), name="register"),
 ]
