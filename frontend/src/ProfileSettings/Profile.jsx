e6567434337c56bf4f6a3ba120c7cc49f2d5b623
abubakar <abubakarbryar@gmail.com>
2026-02-02 04:30:52 +0300

    Improve Front end user flow

--- frontend/src/ProfileSettings/Profile.jsx
+++ frontend/src/ProfileSettings/Profile.jsx
@@ -1,29 +1,32 @@
-import { useHistory } from "react-router-dom"
-import { Button } from "reactstrap"
+import { useHistory } from "react-router-dom";
+import { Button } from "reactstrap";
+import { useQuery } from "@tanstack/react-query";
 import apiClient from "../apiClient";
 
-
 function Profile() {
-  const history  = useHistory();
+  const history = useHistory();
+
+  const { data: user } = useQuery({
+  queryKey: ["users/me"],
+  queryFn: () => apiClient.get("/users/me").then(res => res.data),
+});
 
   const handleLogout = async () => {
     try {
-      await apiClient.post('/logout');
+      await apiClient.post("/logout");
       history.push("/login");
     } catch (error) {
       console.error("Logout error:", error);
     }
-  }
+  };
 
   return (
     <div>
       <h2>Profile Page</h2>
-        <p>Welcome to your profile!</p>
-        <div>
-          <Button onClick={handleLogout}>Logout</Button>
-        </div>
+      <p>Welcome to your profile! {user?.name || user?.email}</p>
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
