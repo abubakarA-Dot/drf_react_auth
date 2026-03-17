e433c1575884988c9998c8c8e9a9965035bde99a
abubakar <abubakarbryar@gmail.com>
2026-03-17 21:39:55 +0300

    Update page design

--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import {  Row, Col, Container } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Page = ({ title, children }) => {
   return (
     <Container className="p-4">
-      <Row>
-        <Col>
-          <h1 className="mb-4">{title}</h1>
-          {children}
-        </Col>
-      </Row>
+      <h3 className="mb-4">{title}</h3>
+      <div className="border p-3 rounded">
+        <Row>
+          <Col>
+            {children}
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
